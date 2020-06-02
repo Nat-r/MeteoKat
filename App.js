3784a4cd2c2e25ca0275d2e1a41a812c1164ec37
Nathan <nrosevegue@edenschool.fr>
2020-06-02 15:45:20 +0200

    Save

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+var color1 = ''
+var color2 = ''
+export default class Meteo extends React.Component {
+  render() {
+    return (
+      <LinearGradient ref={this.Background}
+        colors={[color1, color2]}
+        style={{flex: 1}}
+      >
+      </LinearGradient>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
+
+
+const color = {
+JauneTop : '#fcdc15',
+JauneBot : '#faac40',
+BleuTop : '#3fc4f4',
+BleuBot : '#0096cf',
+GrisTop : '#e4e9e5',
+GrisBot : '#929397',
+VioletTop : '#922790',
+VioletBot : '#662d94'
+}
+var temp = 'Soleil'
+function ChangeColor(event) {
+  
+  if (temp == 'Soleil') {
+    color1 = color.JauneTop;
+    color2 = color.JauneBot;
+  } else if (temp == 'Froid') {
+    color1 = color.BleuTop;
+    color2 = color.BleuBot;
+  }else if (temp == 'Pluie') {
+    color1 = color.GrisTop;
+    color2 = color.GrisBot;
+  }else if (temp == 'Orrage') {
+    color1 = color.VioletTop;
+    color2 = color.VioletBot;
+  }
+}
+ChangeColor();
