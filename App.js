a0ec1c235510c52b0805ac3f2db9eedf5b2cc273
Nathan <nrosevegue@edenschool.fr>
2020-06-08 11:35:16 +0200

    Dev Mode

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Button, Picker } from 'react-native';
+import * as React from 'react'
+import { StyleSheet, Text, View, ScrollView, Image, Button, Picker, Alert } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import RNPickerSelect from 'react-native-picker-select';
+import { CustomPicker } from 'react-native-custom-picker'
+import { NavigationContainer } from '@react-navigation/native';
+import { render } from 'react-dom';
+import { createStackNavigator } from '@react-navigation/stack';
 var color1 = ''
 var color2 = ''
 var icon = ''
@@ -10,50 +13,195 @@ var heure = ''
 var temp = ''
 var temps = ''
 var change = 'Soleil'
-export default class Meteo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      change: undefined,
-      item: [
-        { label: 'Soleil', value: 'Soleil' },
-        { label: 'Neige', value: 'oui' },
-        { label: 'Pluie', value: 'Pluie' },
-        { label: 'Orage', value: 'Orage' },
-        { label: 'Nuage', value: 'Nuage' },
-    ]
-    }
-  }
-  render() {
+
+function Soleil({ navigation  }) {
     return (
-      <LinearGradient ref={this.Background}
+      <LinearGradient
+        colors={[color.JauneTop, color.JauneBot]}
+        style={{flex: 1}}
+      >
+        <ScrollView>
+          <View style={styles.container}>
+            <Text style={styles.textjours}>{donnessun.jours}</Text>
+            <Text style={styles.textheure}>{donnessun.heure}</Text>
+            <View style={styles.imgcont}>
+              <Image style={styles.img} source={icones.sun}/>
+            </View>
+            <View style={styles.textcont}>
+              <Text style={styles.texttemp}>{donnessun.temp}  | <Text style={styles.texttemps}>{donnessun.temps}</Text></Text> 
+            </View>
+            <Button style={styles.btn} title="Dev Mode" onPress={() => navigation.navigate('Dev Mode')} />
+          </View>
+        </ScrollView>
+      </LinearGradient>
+
+    );
+}
+
+function Neige({ navigation  }) {
+  return (
+    <LinearGradient
+      colors={[color.BleuTop, color.BleuBot]}
+      style={{flex: 1}}
+    >
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.textjours}>{donnessnow.jours}</Text>
+          <Text style={styles.textheure}>{donnessnow.heure}</Text>
+          <View style={styles.imgcont}>
+            <Image style={styles.img} source={icones.snow}/>
+          </View>
+          <View style={styles.textcont}>
+            <Text style={styles.texttemp}>{donnessnow.temp}  | <Text style={styles.texttemps}>{donnessnow.temps}</Text></Text> 
+          </View>
+          <Button style={styles.btn} title="Dev Mode" onPress={() => navigation.navigate('Dev Mode')} />
+        </View>
+      </ScrollView>
+    </LinearGradient>
+
+  );
+}
+
+function Pluie({ navigation  }) {
+  return (
+    <LinearGradient
+      colors={[color.GrisTop, color.GrisBot]}
+      style={{flex: 1}}
+    >
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.textjours}>{donnespluie.jours}</Text>
+          <Text style={styles.textheure}>{donnespluie.heure}</Text>
+          <View style={styles.imgcont}>
+            <Image style={styles.img} source={icones.pluie}/>
+          </View>
+          <View style={styles.textcont}>
+            <Text style={styles.texttemp}>{donnespluie.temp}  | <Text style={styles.texttemps}>{donnespluie.temps}</Text></Text> 
+          </View>
+          <Button style={styles.btn} title="Dev Mode" onPress={() => navigation.navigate('Dev Mode')} />
+        </View>
+      </ScrollView>
+    </LinearGradient>
+
+  );
+}
+
+function Orage({ navigation  }) {
+  return (
+    <LinearGradient
+      colors={[color.VioletTop, color.VioletBot]}
+      style={{flex: 1}}
+    >
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.textjours}>{donnesorage.jours}</Text>
+          <Text style={styles.textheure}>{donnesorage.heure}</Text>
+          <View style={styles.imgcont}>
+            <Image style={styles.img} source={icones.orage}/>
+          </View>
+          <View style={styles.textcont}>
+            <Text style={styles.texttemp}>{donnesorage.temp}  | <Text style={styles.texttemps}>{donnesorage.temps}</Text></Text> 
+          </View>
+          <Button style={styles.btn} title="Dev Mode" onPress={() => navigation.navigate('Dev Mode')} />
+        </View>
+      </ScrollView>
+    </LinearGradient>
+
+  );
+}
+
+function Nuage({ navigation  }) {
+  return (
+    <LinearGradient
+      colors={[color.VertTop, color.VertBot]}
+      style={{flex: 1}}
+    >
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={styles.textjours}>{donnesnuage.jours}</Text>
+          <Text style={styles.textheure}>{donnesnuage.heure}</Text>
+          <View style={styles.imgcont}>
+            <Image style={styles.img} source={icones.nuage}/>
+          </View>
+          <View style={styles.textcont}>
+            <Text style={styles.texttemp}>{donnesnuage.temp}  | <Text style={styles.texttemps}>{donnesnuage.temps}</Text></Text> 
+          </View>
+          <Button style={styles.btn} title="Dev Mode" onPress={() => navigation.navigate('Dev Mode')} />
+        </View>
+      </ScrollView>
+    </LinearGradient>
+
+  );
+}
+
+function Meteo({ navigation }){
+    return (
+      <LinearGradient 
         colors={[color1, color2]}
         style={{flex: 1}}
       >
-        <View style={styles.container}>
-          <Text style={styles.textjours}>{jours}</Text>
-          <Text style={styles.textheure}>{heure}</Text>
-          <View style={styles.imgcont}>
+        <ScrollView>
+          <View style={styles.container}>
+            <Text style={styles.textjours}>{jours}</Text>
+            <Text style={styles.textheure}>{heure}</Text>
+            <View style={styles.imgcont}>
             <Image style={styles.img} source={icon}/>
           </View>
           <View style={styles.textcont}>
             <Text style={styles.texttemp}>{temp}  | <Text style={styles.texttemps}>{temps}</Text></Text> 
           </View>
-        </View>
-        <RNPickerSelect
-        onValueChange={(value) => {
-          this.setState({
-              change: value,
-          });
-        }}
-        items={this.state.item}
-        value={this.state.change}
-        />
+            <Button style={styles.btn} title="Dev Mode" onPress={() => navigation.navigate('Dev Mode')} />
+          </View>
+        </ScrollView>
       </LinearGradient>
     );
   }
+function Dev({ navigation }) {
+  return (
+    <LinearGradient 
+        colors={[color1, color2]}
+        style={{flex: 1}}
+      >
+        <View style={styleHome.container}>
+          <View style={styleHome.conth1}>
+            <Text style={styleHome.h1}>Dev Mode</Text>
+          </View> 
+          <View>
+            <Button title="Meteo" onPress={() => navigation.navigate('Home')} />
+            <Text>&nbsp;</Text>
+            <Button title="Style Soleil" onPress={() => navigation.navigate('Soleil')} />
+            <Text>&nbsp;</Text>
+            <Button title="Style Neige" onPress={() => navigation.navigate('Neige')} />
+            <Text>&nbsp;</Text>
+            <Button title="Style Pluie" onPress={() => navigation.navigate('Pluie')} />
+            <Text>&nbsp;</Text>
+            <Button title="Style Orage" onPress={() => navigation.navigate('Orage')} />
+            <Text>&nbsp;</Text>
+            <Button title="Style Nuage" onPress={() => navigation.navigate('Nuage')} />
+          </View>
+        </View>
+
+    </LinearGradient>
+  );
 }
 
+const Stack = createStackNavigator();
+export default function App(){
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+        <Stack.Screen name="Home" component={Meteo} />
+        <Stack.Screen name="Dev Mode" component={Dev} />
+        <Stack.Screen name="Soleil" component={Soleil} />
+        <Stack.Screen name="Neige" component={Neige} />
+        <Stack.Screen name="Pluie" component={Pluie} />
+        <Stack.Screen name="Orage" component={Orage} />
+        <Stack.Screen name="Nuage" component={Nuage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     paddingLeft: 20,
@@ -75,7 +223,7 @@ const styles = StyleSheet.create({
   },
   imgcont:{
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center' 
   },
   img: {
     marginTop: 50,
@@ -84,7 +232,7 @@ const styles = StyleSheet.create({
   },
   texttemp: {
     fontSize: 55,
-    marginTop: 400,
+    marginTop: 70,
     padding: 0,
     margin: 0,
     color: 'white',
@@ -96,7 +244,8 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   textcont : {
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingBottom : 200 
   },
   picker : {
     width: 150,
@@ -105,9 +254,31 @@ const styles = StyleSheet.create({
   pickernone : {
     width: 150,
     display: "none",
+  },
+  btn : {
+    
   }
 });
 
+const styleHome = StyleSheet.create({
+  container : {
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  conth1 : {
+    paddingTop : 200,
+    marginBottom : 70,
+    alignItems: 'center'
+  },
+  h1 : {
+    color : 'white',
+    fontSize : 50,
+  },
+  btn : {
+    flex: 1,
+    justifyContent : 'space-between'
+  },
+});
 
 
 
@@ -161,12 +332,11 @@ const donnesnuage = {
   temps : 'Nuage',
 }
 var test = 'Pluie'
-
-function ChangeTheme(event) {
- change = event;
- ChangeColor();
+function changeTheme(event) {
+  change = event;
+  //alert(change)
+  ChangeColor();
 }
-
 function ChangeColor(event) {
   
   if (change == 'Soleil') {
